e14c6d65a40b705dd2230a8d5f023e3d799f772e
asprinkleofcode <alisha.sprinkle.korba@gmail.com>
2025-10-12 14:12:40 -0400

    Flowbite configured

--- asprinkleofcode/src/App.jsx
+++ asprinkleofcode/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Button } from "flowbite-react";
+import { Datepicker } from "flowbite-react";
+import { Progress } from "flowbite-react";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div class="bg-berry">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+ 
     </>
   )
 }
